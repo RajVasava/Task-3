a72fc6a8efe36726ecbd46eb9c4330f2f93f715d
Raj Vasava <rajbca65a@gmail.com>
2026-03-22 14:09:08 +0530

    Add files via upload

---
+++ Backend/models/Document.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const DocumentSchema = new mongoose.Schema({
+  _id: String,       // document ID
+  content: Object,   // Quill stores content as JSON
+});
+
+module.exports = mongoose.model("Document", DocumentSchema);
+
+
